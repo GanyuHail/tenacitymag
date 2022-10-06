7fb054bd9dc6d2fd614903c501038401343580c8
HailGanyu <henryyanguk@gmail.com>
2022-10-06 12:23:04 +0100

    comment out sphere

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import GLTFLoader from 'three-gltf-loader';
 
 function App() {
   useEffect(() => {
@@ -31,11 +32,26 @@ function App() {
     spotLight.position.set(0, 64, 32);
     scene.add(spotLight);
 
-    const sphereGeometry = new THREE.SphereGeometry(12, 64, 32);
-    const sphereTexture = new THREE.TextureLoader().load('https://raw.githubusercontent.com/GanyuHail/3dArt/main/uniPinch1.jpg');
-    const sphereMaterial = new THREE.MeshBasicMaterial({ map: sphereTexture });
-    const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
-    scene.add(sphereMesh);
+    //const sphereGeometry = new THREE.SphereGeometry(12, 64, 32);
+    //const sphereTexture = new THREE.TextureLoader().load('https://raw.githubusercontent.com/GanyuHail/3dArt/main/uniPinch1.jpg');
+    //const sphereMaterial = new THREE.MeshBasicMaterial({ map: sphereTexture });
+    //const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    //scene.add(sphereMesh);
+
+    const loader = new GLTFLoader();
+    loader.load('baesLogo1.gltf',
+  
+      function (gltf) {
+
+        scene.add(gltf.scene);
+
+        gltf.animations; // Array<THREE.AnimationClip>
+        gltf.scene; // THREE.Group
+        gltf.scenes; // Array<THREE.Group>
+        gltf.cameras; // Array<THREE.Camera>
+        gltf.asset; // Object
+      }
+    );
 
     sphereGeometry.userData = { URL: "https://github.com/GanyuHail/3dArt/blob/main/Hi%20Res%20-.jpg" };
 
