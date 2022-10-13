a8ddc5aab63233bcaddb199a97682eaa5fd0416b
HailGanyu <henryyanguk@gmail.com>
2022-10-13 12:53:41 +0100

    Updates

--- assets/index.a4cd2bbb.js
+++ assets/index.a4cd2bbb.js
@@ -35208,8 +35208,8 @@ var _GLTFLoader = function() {
       }
       loader.load(url, function(data) {
         try {
-          scope.parse(data, resourcePath, function(gltf) {
-            onLoad(gltf);
+          scope.parse(data, resourcePath, function(gltf2) {
+            onLoad(gltf2);
             scope.manager.itemEnd(url);
           }, _onError);
         } catch (e) {
@@ -37537,8 +37537,8 @@ function App() {
     spotLight.physicallyCorrectLights = true;
     scene.add(spotLight);
     const loader = new threeGltfLoader().setPath("https://raw.githubusercontent.com/GanyuHail/bl3/main/src/");
-    const baes = loader.load("baesLogoMaster4.gltf", function(gltf) {
-      scene.add(gltf.scene);
+    loader.load("baesLogoMaster4.gltf", function(gltf2) {
+      scene.add(gltf2.scene);
     });
     document.addEventListener("mousedown", onMouseDown);
     function onMouseDown(event) {
@@ -37546,7 +37546,7 @@ function App() {
       const mouse3D = new Vector3(event.clientX / window.innerWidth * 2 - 1, -(event.clientY / window.innerheight) * 2 - 1, 0.5);
       const raycaster = new Raycaster();
       raycaster.setFromCamera(mouse3D, camera);
-      const intersects2 = raycaster.intersectObjects(baes);
+      const intersects2 = raycaster.intersectObjects(gltf.scene);
       if (intersects2.length > 0) {
         console.log();
       }
