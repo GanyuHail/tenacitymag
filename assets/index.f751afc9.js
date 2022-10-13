a620d0ef6bab2d0b96ba7300606799f7d9bb64cc
HailGanyu <henryyanguk@gmail.com>
2022-10-13 13:16:51 +0100

    Updates

--- assets/index.f751afc9.js
+++ assets/index.f751afc9.js
@@ -37539,17 +37539,14 @@ function App() {
     const loader = new threeGltfLoader().setPath("https://raw.githubusercontent.com/GanyuHail/bl3/main/src/");
     loader.load("baesLogoMaster4.gltf", function(gltf2) {
       scene.add(gltf2.scene);
-      gltf2.scene;
-      console.log();
     });
     document.addEventListener("mousedown", onMouseDown);
     function onMouseDown(event) {
       event.preventDefault();
-      const baes = gltf.scene;
       const mouse3D = new Vector3(event.clientX / window.innerWidth * 2 - 1, -(event.clientY / window.innerheight) * 2 - 1, 0.5);
       const raycaster = new Raycaster();
       raycaster.setFromCamera(mouse3D, camera);
-      const intersects2 = raycaster.intersectObjects(baes);
+      const intersects2 = raycaster.intersectObjects(gltf, true);
       if (intersects2.length > 0) {
         console.log("click!");
       }
