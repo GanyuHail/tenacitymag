0569e83f6122238a14b886ccb11e4f7b0ac14671
HailGanyu <henryyanguk@gmail.com>
2022-10-28 12:39:36 +0100

    upload logo5

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ function App() {
     scene.add(spotLight);
 
     const loader = new GLTFLoader().setPath('https://raw.githubusercontent.com/GanyuHail/bl3/main/src/');
-    loader.load('baesLogoMaster4.gltf', function (gltf) {
+    loader.load('baesLogoMaster5.gltf', function (gltf) {
       scene.add(gltf.scene);
     });
 
