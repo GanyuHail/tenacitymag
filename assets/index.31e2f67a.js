75e30b689285a7f659c195938cc8cfd984794bd6
HailGanyu <henryyanguk@gmail.com>
2023-05-31 12:26:11 +0100

    Updates

--- assets/index.31e2f67a.js
+++ assets/index.31e2f67a.js
@@ -37899,9 +37899,9 @@ function App() {
     };
     const scene = new Scene();
     const camera = new PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 500);
-    camera.position.x = 1;
-    camera.position.z = 1;
-    camera.position.y = 1;
+    camera.position.x = 0;
+    camera.position.z = 0;
+    camera.position.y = 10;
     camera.lookAt(0, 0, 0);
     const canvas = document.getElementById("myThreeJsCanvas");
     const renderer = new WebGLRenderer({
